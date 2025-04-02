871e0d52bb61e17bc89669393a85e52ea005878b
Nkosinathi Tshabalala <Nkosinathit217@gmail.com>
2025-04-02 13:28:07 +0200

    added ascii art and audioplayer functionality

--- src/main/Java/AudioPlayer.java
+++ src/main/Java/AudioPlayer.java
@@ -1,3 +1,19 @@
+import javax.sound.sampled.*;
+import java.io.InputStream;
+
 public class AudioPlayer {
-  
+    public static void playWelcomeSound() {
+        try {
+            InputStream audioStream = AudioPlayer.class.getResourceAsStream("/welcome.wav");
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(audioStream);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+            
+            // Let the audio play completely
+            Thread.sleep(clip.getMicrosecondLength() / 1000);
+        } catch (Exception e) {
+            System.out.println("Audio error: " + e.getMessage());
+        }
+    }
 }
